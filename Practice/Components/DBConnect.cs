427b164e5b391e1a51c3d21ef8cdbe27b3821a4e
Medina <Medina@LAPTOP>
2022-12-02 18:16:49 +0300

    Добавьте файлы проекта.

---
+++ Practice/Components/DBConnect.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice.Components
+{
+    internal class DBConnect
+    {
+        public static ProductsEntities3 db = new ProductsEntities3();
+    }
+}
